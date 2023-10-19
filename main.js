dbb2694f3bc8220113f6412bfae8164e6babc1d4
Luis Puig <lpuig89@gmail.com>
2023-10-19 23:17:31 +0200

    Add ArrayFunctions class and fixed functions

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { myLength, myPop, myPush, myShift } from "./modules/arrays.js";
+import { ArrayFunctions } from "./modules/arrays.js";
 
 const array1 = [];
 const array2 = [2, 5, 8, 6];
-const a = myPush(array1, 4);
+const a = ArrayFunctions.myPush(array1, 4);
 console.log(a);
-console.log(myShift(array2));
-myLength(array1);
-myPop(array2);
+console.log(ArrayFunctions.myShift(array2));
+ArrayFunctions.myLength(array1);
+ArrayFunctions.myPop(array2);
